5896ce336485b8d52aeadf0a75650d99928caf2c
Rubin Thomas <rubinthomas@rubinthomas.com>
2015-08-12 09:32:28 +1200

    minor Update
    minor Update

--- cmis_field/js/cmis_field.js
+++ cmis_field/js/cmis_field.js
@@ -2,8 +2,6 @@
 
   Drupal.behaviors.cmisField = {
     attach: function(context, settings) {
-      //TODO: This selector doesn't seem right - will it work if there's a different default lang.?
-      //$("#edit-field-cmis-und-0-title", context).after('<input type="button" id="edit-cmis-field" class="cmis-field-insert-button form-submit" value="Browse" />');
       $(".edit-field-cmis-field", context).after('<input type="button" id="edit-cmis-field" class="cmis-field-insert-button form-submit" value="Browse" />');
       $("#edit-instance-settings-cmis-field-rootfolderpath", context).after('<input type="button" id="edit-cmis-settings" class="cmis-field-insert-button form-submit" value="Browse" />');
       $('.cmis-field-insert-button').click(function() {
@@ -16,15 +14,15 @@
         }
         var rootDir = Drupal.settings.cmispath;
         //TODO: Make this a modal, not a popup (will be much harder to manage context when clicking around the cmis browser)
-        if (rootDir == true){
-	      rootDir === '';
-	      window.open('/cmis/browser?type=popup&caller=' + caller, 'cmisBrowser', 'width=800,height=500,resizable');
-        }else{
+        if (rootDir == true) {
+          rootDir === '';
+          window.open('/cmis/browser?type=popup&caller=' + caller, 'cmisBrowser', 'width=800,height=500,resizable');
+        } else {
           window.open('/cmis/browser' + rootDir + '?type=popup&caller=' + caller, 'cmisBrowser', 'width=800,height=500,resizable');
         }
         return false;
       });
-      
+
       $(".cmis-field-insert", context).click(function() {
         if($.query['caller'] == 'settings') {
           var cmispath = $(this).attr('href');
@@ -33,7 +31,6 @@
         else {
           var cmispath = $(this).attr('id');
           var cmisname = $(this).attr('name');
-          //$('#edit-field-cmis-und-0-title', window.opener.document).val(cmisname);          
           $('.edit-field-cmis-field', window.opener.document).val(cmisname);
           $('.edit-field-cmis-path', window.opener.document).val(cmispath);
         }
@@ -42,7 +39,7 @@
       });
     }
   };
-  
+
   $.query = (function(a) {
     if (a == "") return {};
     var b = {};
@@ -52,5 +49,6 @@
         b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
     }
     return b;
-})(window.location.search.substr(1).split('&'))
+  })(window.location.search.substr(1).split('&'))
+
 })(jQuery);
